1cfb48ba30f6a7d0bfae858c28734d237350e7c3
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-11-05 14:50:38 +0100

    Completata Milestone 1

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -7,5 +7,40 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::with('type', 'technologies')->paginate(5);
+
+        return response()->json([
+            'success' => true,
+            'code' => 200,
+            'message' => 'Funziona',
+            'data' => [
+                'projects' => $projects
+            ],
+        ]);
+    }
+
+    public function show(string $slug)
+    {
+        $projects = Project::with('type', 'technologies')->where('slug', $slug)->firstOrFail();
+
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'code' => 200,
+                'message' => 'Funziona',
+                'data' => [
+                    'projects' => $projects
+                ],
+            ]);
+        }
+        else {
+            return response()->json([
+                'success' => false,
+                'code' => 404,
+                'message' => 'Progetto non Trovato!'
+            ]);
+        }
+    }
 }
